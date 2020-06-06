ee2e2903fcf35e432b9b0ed55ac496a7ecaf7fee
KaiXtr <42985693+KaiXtr@users.noreply.github.com>
2020-06-06 01:38:43 -0300

    Added book
    - Canaã

---
+++ books/000015.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+
+<html>
+	<!--Então você gosta de usar o botão inspecionar né...?-->
+	<head>
+		<?php $v = 'Canaã - '; include '../design/metadata.php'; ?>
+	</head>
+
+	<body>
+		<?php include '../design/header.php' ?>
+		<?php include '../design/lateralbar.php' ?>
+
+		<div id='bookinfo'>
+			<?php $book = '000015'; $ech = '1'; include '../design/bookinfo.php'; ?>
+			<div id='review'>
+				<div class='manlan' lang='pt'>
+					<a name='goto1'></a>
+					<h1> Resenha </h1>
+					Canaã[1] é um livro de Graça Aranha publicado no Brasil pela primeira vez em 1902. O romance-novela aborda a imigração alemã no estado do Espírito Santo, por intermédio do conflito entre dois personagens principais, Milkau e Lentz, que representam diferentes linhas filosóficas.[2]
+
+					Temas como opressão feminina, imperialismo germânico, militarismo, corrupção dos administradores públicos, ostracismo, conflito de adaptação à nova terra são tratados nesse romance.[3]
+					<hr>
+					<a name='goto2'></a>
+					<h1> Personagens </h1>
+					<ul>
+					<li><b>Milkau: </b>.</li>
+					<li><b>Lentz: </b>.</li>
+					<li><b>Maria: </b>.</li>
+					</ul>
+					<hr>
+				</div>
+			</div>
+			<?php $book = '000015'; $ech = '2'; include '../design/bookinfo.php'; ?>
+		</div>
+		<?php $user = 'GRACAaranha'; include '../design/auctorbooks.php'; ?>
+
+		<?php include '../design/footer.php' ?>
+	</body>
+</html>
---
+++ sinopsis/000015.php
@@ -0,0 +1,9 @@
+<div class='manlan' lang='pt'>
+Tanto Milkau quanto Lentz são jovens imigrantes alemães que vieram para o Espírito Santo, e após uma falsa acusação de assassinato, os dois partem para Canaã.
+</div>
+<div class='manlan' lang='en'>
+Both Milkau and Lentz are young German immigrants who came to Espírito Santo, and after a false accusation of murder, the two leave for Canaan.
+</div>
+<div class='manlan' lang='es'>
+Tanto Milkau como Lentz son jóvenes inmigrantes alemanes que vinieron a Espírito Santo, y después de una falsa acusación de asesinato, los dos se van a Canaán.
+</div>
